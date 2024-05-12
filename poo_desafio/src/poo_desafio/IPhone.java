cce839777b647ff2ea0afed86c4341aed323a8f7
Matheus Felix <matheusfelix.amorim@hotmail.com>
2024-05-12 15:47:07 -0300

    Adicionar arquivos

---
+++ poo_desafio/src/poo_desafio/IPhone.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poo_desafio;
+
+/**
+ *
+ * @author Matheus
+ */
+public class IPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    @Override
+    public void tocar() {
+        // Implementação
+    }
+
+    @Override
+    public void pausar() {
+        // Implementação
+    }
+
+    @Override
+    public void selecionarMusica() {
+        // Implementação
+    }
+
+    @Override
+    public void ligar() {
+        // Implementação
+    }
+
+    @Override
+    public void atender() {
+        // Implementação
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        // Implementação
+    }
+
+    @Override
+    public void exibirPagina() {
+        // Implementação
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        // Implementação
+    }
+
+    @Override
+    public void atualizarPagina() {
+        // Implementação
+    }
+}
